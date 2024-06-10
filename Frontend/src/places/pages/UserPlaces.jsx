ba2c357d35632026f40919a22c8ed10bcb9c52bc
xtsebas <thiagohugo2014@gmail.com>
2024-06-10 16:22:48 -0600

    feat: modal created and added to placeitems

--- Frontend/src/places/pages/UserPlaces.jsx
+++ Frontend/src/places/pages/UserPlaces.jsx
@@ -4,9 +4,9 @@ import { useParams } from 'react-router-dom';
 import PlaceList from '../components/PlaceList';
 
 const DummyPlaces = [
-    { id: 'p1', title: 'Place One', description: 'This is place one.', imageUrl: 'https://s1.significados.com/foto/paisaje-og.jpg?class=ogImageWide', addres: '1234 Fake St', creatorId: 'u1', location: { lat: 40.748817, lng: -73.985428 }},
-    { id: 'p2', title: 'Place Two', description: 'This is place two.', imageUrl: 'https://s1.significados.com/foto/paisaje-og.jpg?class=ogImageWide', addres: '1234 Fake St', creatorId: 'u2', location: { lat: 40.748817, lng: -73.985428 } },
-    { id: 'p3', title: 'Place Three', description: 'This is place three.', imageUrl: 'https://s1.significados.com/foto/paisaje-og.jpg?class=ogImageWide', addres: '1234 Fake St', creatorId: 'u1', location: { lat: 40.748817, lng: -73.985428 } },
+    { id: 'p1', title: 'Place One', description: 'This is place one.', imageUrl: 'https://s1.significados.com/foto/paisaje-og.jpg?class=ogImageWide', address: '1234 Fake St', creatorId: 'u1', location: { lat: 40.748817, lng: -73.985428 }},
+    { id: 'p2', title: 'Place Two', description: 'This is place two.', imageUrl: 'https://s1.significados.com/foto/paisaje-og.jpg?class=ogImageWide', address: '1234 Fake St', creatorId: 'u2', location: { lat: 40.748817, lng: -73.985428 } },
+    { id: 'p3', title: 'Place Three', description: 'This is place three.', imageUrl: 'https://s1.significados.com/foto/paisaje-og.jpg?class=ogImageWide', address: '1234 Fake St', creatorId: 'u1', location: { lat: 40.748817, lng: -73.985428 } },
 ];
 
 const UserPlaces = props => {
